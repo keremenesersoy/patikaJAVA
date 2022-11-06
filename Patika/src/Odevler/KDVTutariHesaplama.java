0c8f41f21dfdb5b23199dd92399a40155a91a00f
Kerem Enes Ersoy <73962938+keremenesersoy@users.noreply.github.com>
2022-11-06 22:09:29 +0300

    Add files via upload

---
+++ Patika/src/Odevler/KDVTutariHesaplama.java
@@ -0,0 +1,27 @@
+package Odevler;
+
+import java.util.Scanner;
+
+public class KDVTutariHesaplama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double kdv = 18;
+		
+		Scanner degerAl = new Scanner(System.in);
+		
+		System.out.print("Miktar Giriniz = ");
+		double fiyat = degerAl.nextDouble();
+		
+		double kdvFiyat = fiyat + (fiyat * (kdv / 100));
+		
+		double sonuc = kdvFiyat - fiyat;
+		
+		System.out.println("KDV \'siz Fiyat = " + fiyat);
+		System.out.println("KDV \'li Fiyat = " + kdvFiyat);
+		System.out.println("KDV Tutari = " + sonuc);
+		
+	}
+
+}
